c8403f8ef1a12cb55ae9ab4c282a7d5b306dd24b
Tássio Virgínio <tassiovirginio@gmail.com>
2021-07-08 14:15:30 -0300

    first commit

--- src/main/java/com/tassiovirginio/wicketstart/HomePage.scala
+++ src/main/java/com/tassiovirginio/wicketstart/HomePage.scala
@@ -5,6 +5,6 @@ import org.apache.wicket.markup.html.basic.Label
 import org.apache.wicket.markup.html.WebPage
 
 @SerialVersionUID(1L)
-class HomePage(val parameters: PageParameters) extends WebPage(parameters) {
+class HomePage extends WebPage {
   add(new Label("version", getApplication.getFrameworkSettings.getVersion))
 }
